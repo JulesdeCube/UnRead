9cc03f520a937243b00be28e29f6e9f7b844104f
zeppone <61550499+zeppone@users.noreply.github.com>
2020-10-24 20:11:53 +0200

    Update segmentation.h

--- src/load_Image/segmentation.h
+++ src/load_Image/segmentation.h
@@ -9,16 +9,22 @@
 #include <string.h>
 #include <dirent.h>
 
-struct s_int_array
-{
-    int *values;
-    size_t size;
-};
+//create an array of the horizontal projection of a pixbuf image. Each array[i] link to the sum of black pixel of the 'i' line.
 int * horizontalProjection(GdkPixbuf *pixbuf);
+
+//create an array of the vertical projection of a pixbuf image. Each array[i] link to the sum of black pixel of the 'i' collum.
 int * verticalProjection(GdkPixbuf *pixbuf);
+
+//take a pixbuf image and create crop each line in a specific directory
 void lineSegmentation(GdkPixbuf *pixbuf);
+
+//take a pixbuf image and create crop each character in a specific directory
 void charSegmentation(GdkPixbuf *pixbuf,int lineNumber);
+
+//sub function used to create a gtk widget from a file
 GtkWidget* create_image2 (char path[255]);
+
+//main segmentation, use line Segmentation on the pixbuf image. Then use charSegmentation on each line.
 void mainSegmentation(GtkWidget* image_to_change);
 
 
